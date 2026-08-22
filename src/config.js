baa3a115791d88f620c63609bce8950982fd6db6
Santiago <santiagoencinas38_email@ejemplo.com>
2026-08-23 00:00:52 +0200

    tarea 5: agregar informacion del estudiante

--- src/config.js
+++ src/config.js
@@ -3,4 +3,5 @@ export const config = {
   environment: process.env.APP_ENV ?? 'development',
   eventName: process.env.EVENT_NAME ?? 'TechFest UCB',
   maxWorkshops: Number(process.env.MAX_WORKSHOPS ?? 3),
+  studentName: process.env.STUDENT_NAME ?? 'Estudiante UCB',
 };
